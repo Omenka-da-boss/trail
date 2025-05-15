a26c63edbde222f97e64f1f91dc6984c194aa94e
Omenka-da-boss <omenkandubuisi@gmail.com>
2025-05-15 10:12:10 +0100

    change

--- wow.py
+++ wow.py
@@ -1,4 +1,28 @@
-print("Hello World")
+# print("Hello World")
 
-name =  input("Enter your name: ")
-print(f"Welcome back {name}")
+# name =  input("Enter your name: ")
+# print(f"Welcome back {name}")
+
+
+class Animal:
+    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+    def hello(self):
+        print(f"Hello {self.name}")
+    
+    def goodbye(self):
+        print(f"Goodbye {self.name}")
+    
+    def check(self):
+        if self.age >= 20:
+            print(f"Welcome to this page you are eliglbe")
+        else:
+             print(f"Come back when you are up to 20")
+
+student = Animal("Omenka",22)
+
+print(student.name)
+student.check()
